82e60cae4aaa2616b5d02c741c2892e640dd6cd9
Zheng Xu <zhengxu7806@gmail.com>
2025-04-30 19:35:04 -0400

    expose singleton

--- src/wysebee/__init__.py
+++ src/wysebee/__init__.py
@@ -2,3 +2,4 @@ from .core.wysebee import Wysebee
 from .core.wysebee_webview import WysebeeWebView
 from .core.wysebee_webengine_page import WysebeeWebEnginePage
 from .core.wysebee_backend import WysebeeBackend
+from .core.singleton import singleton
